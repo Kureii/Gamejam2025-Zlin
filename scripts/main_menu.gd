c5aba81e731bfb616dc68a9ef6e1f256c7de2f36
kureii (Tomáš Adámek) <ad.tomik@seznam.cz>
2025-03-15 05:44:49 +0100

    fix main menu

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -12,22 +12,22 @@ func _on_start_game_button_up() -> void:
 func _on_create_level_button_up() -> void:
 	pass # Replace with function body.
 
-
-func _on_hall_of_flame_button_up() -> void:
-	var credits = get_tree().get_root().get_node("MainMenu/Aspect/Hall_of_flame")
-	credits.visible = true
-
-
-func _on_credits_button_up() -> void:
-	var credits = get_tree().get_root().get_node("MainMenu/Aspect/Credits")
-	credits.visible = true
-
-
 func _on_hide_credits_button_up() -> void:
 	var credits = get_tree().get_root().get_node("MainMenu/Aspect/Credits")
 	credits.visible = false
 
+func _on_credits_button_up() -> void:
+	var credits = get_tree().get_root().get_node("MainMenu/Aspect/Credits")
+	var hall = get_tree().get_root().get_node("MainMenu/Aspect/Hall_of_flame")
+	credits.visible = true
+	hall.visible = false
+
+func _on_hall_of_flame_button_up() -> void:
+	var hall = get_tree().get_root().get_node("MainMenu/Aspect/Hall_of_flame")
+	var credits = get_tree().get_root().get_node("MainMenu/Aspect/Credits")
+	hall.visible = true
+	credits.visible = false
 
 func _on_hide_hall_of_flame_button_up() -> void:
-	var credits = get_tree().get_root().get_node("MainMenu/Aspect/Hall_of_flame")
-	credits.visible = false
+	var hall = get_tree().get_root().get_node("MainMenu/Aspect/Hall_of_flame")
+	hall.visible = false
