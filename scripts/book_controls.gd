46cf63691fa0a0b2ac94e50fedc8d41076c05c51
kureii (Tomáš Adámek) <ad.tomik@seznam.cz>
2025-03-16 11:33:59 +0100

    book page by click

--- scripts/book_controls.gd
+++ scripts/book_controls.gd
@@ -2,4 +2,5 @@ extends MarginContainer
 
 func _on_book_visibility_changed() -> void:
 	var book = %Book
-	visible = book.visible
+	var container = $MarginContainer/HBoxContainer
+	container.visible = book.visible
--- scripts/camera_3d.gd
+++ scripts/camera_3d.gd
@@ -2,10 +2,12 @@ extends Camera3D
 
 @export var rotate_speed: float = 5
 
+var camera_can_move: bool = true
 var rot_vector_ : Vector3 = Vector3(0,0,0)
 
 func _process(delta: float) -> void:
-	cam_rotate(delta)
+	if camera_can_move:
+		cam_rotate(delta)
 	
 func cam_rotate(delta: float) -> void:
 	if Input.is_action_pressed("ui_left"):
@@ -18,3 +20,11 @@ func cam_rotate(delta: float) -> void:
 		rot_vector_ += Vector3(delta * rotate_speed * -0.5,0,0)
 	
 	rotation = rot_vector_ 
+
+
+func _on_prew_button_button_up() -> void:
+	%Book.get_child(0).prew_page()
+
+
+func _on_next_button_button_up() -> void:
+	%Book.get_child(0).next_page()
